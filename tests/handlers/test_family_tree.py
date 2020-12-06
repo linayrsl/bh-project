b61ad0c27b61d202782c60e10621c1b09089677e
Lina Yaroslavski <lina.yrsl@gmail.com>
2020-12-06 15:00:16 +0200

    Fixes test to fit new family tree data structure

--- tests/handlers/test_family_tree.py
+++ tests/handlers/test_family_tree.py
@@ -12,26 +12,55 @@ application.config['TESTING'] = True
 blank_image = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAIBAQEBAQIBAQECAgICAgQDAgICAgUEBAMEBgUGBgYFBgYGBwkIBgcJBwYGCAsICQoKCgoKBggLDAsKDAkKCgr/2wBDAQICAgICAgUDAwUKBwYHCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgr/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD+f+iiigD/2Q=="
 
 test_family_tree: dict = {
-    "submitterEmail": "email@server.com",
-    "familyTree": {
-        "1": {
-            "ID": "1",
-            "firstName": "Alex",
-            "lastName": "Smith",
-            "maidenName": None,
-            "birthDate": "10/02/1955",
-            "birthPlace": "New York",
-            "gender": "male",
-            "motherID": "10",
-            "fatherID": "20",
-            "isAlive": False,
-            "deathPlace": "Boston",
-            "deathDate": "07/07/2015",
-            "image": blank_image,
-            "siblings": ["1000", "2000", "3000"]
-        }
+  "language": "he",
+  "submitter": {
+    "image": None,
+    "firstName": "פלונית",
+    "lastName": "אלמונית",
+    "maidenName": None,
+    "gender": "female",
+    "birthDate": "18/12/1983",
+    "birthPlace": "טשקנט",
+    "isAlive": True,
+    "deathDate": None,
+    "deathPlace": None,
+    "isSubmitter": True,
+    "mother": {
+      "image": None,
+      "firstName": "פלונה",
+      "lastName": "אלמונה",
+      "maidenName": "בלהבלה",
+      "gender": "female",
+      "birthDate": "1964",
+      "birthPlace": "טשקנט",
+      "isAlive": True,
+      "deathDate": None,
+      "deathPlace": None,
+      "isSubmitter": False,
+      "mother": None,
+      "father": {
+        "image": None,
+        "firstName": "פלוני",
+        "lastName": "אלמוני",
+        "maidenName": None,
+        "gender": "male",
+        "birthDate": None,
+        "birthPlace": None,
+        "isAlive": True,
+        "deathDate": None,
+        "deathPlace": None,
+        "isSubmitter": False,
+        "mother": None,
+        "father": None,
+        "siblings": None
+      },
+      "siblings": []
     },
-    "language": "he"
+    "father": None,
+    "siblings": [],
+    "children": []
+  },
+  "submitterEmail": "test@gmail.com"
 }
 
 
@@ -49,7 +78,7 @@ class TestFamilyTree(unittest.TestCase):
 
     def test_family_tree_bad_first_name(self):
         family_tree = copy.deepcopy(test_family_tree)
-        family_tree["familyTree"]["1"]["firstName"] = 123
+        family_tree["submitter"]["firstName"] = 123
 
         with application.test_client() as client:
             response: Response = client.post("/api/family-tree/", json=family_tree)
@@ -58,7 +87,7 @@ class TestFamilyTree(unittest.TestCase):
 
     def test_family_tree_bad_birth_date(self):
         family_tree = copy.deepcopy(test_family_tree)
-        family_tree["familyTree"]["1"]["birthDate"] = "18-12-1983"
+        family_tree["submitter"]["birthDate"] = "18-12-1983"
 
         with application.test_client() as client:
             response: Response = client.post("/api/family-tree/", json=family_tree)
@@ -67,7 +96,7 @@ class TestFamilyTree(unittest.TestCase):
 
     def test_family_tree_missing_value(self):
         family_tree = copy.deepcopy(test_family_tree)
-        family_tree["familyTree"]["1"]["gender"] = ""
+        family_tree["submitter"]["gender"] = ""
 
         with application.test_client() as client:
             response: Response = client.post("/api/family-tree/", json=family_tree)
@@ -76,7 +105,7 @@ class TestFamilyTree(unittest.TestCase):
 
     def test_family_tree_incorrect_gender_choice(self):
         family_tree = copy.deepcopy(test_family_tree)
-        family_tree["familyTree"]["1"]["gender"] = "animal"
+        family_tree["submitter"]["gender"] = "animal"
 
         with application.test_client() as client:
             response: Response = client.post("/api/family-tree/", json=family_tree)
@@ -85,7 +114,7 @@ class TestFamilyTree(unittest.TestCase):
 
     def test_family_tree_missing_required_field(self):
         family_tree = copy.deepcopy(test_family_tree)
-        family_tree["familyTree"]["1"] = ""
+        family_tree["submitter"] = ""
 
         with application.test_client() as client:
             response: Response = client.post("/api/family-tree/", json=family_tree)
